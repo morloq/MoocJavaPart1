353976f8af03b7231d6cd11b0df45e9ba29ab34c
Sara <75135633+morloq@users.noreply.github.com>
2022-07-10 17:39:34 +0200

    Add files via upload

---
+++ week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,14 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        // Write your program here
+        int x = 1;
+        while (x <= 100)
+        {
+            System.out.println(x);
+            x++;
+            
+        }
+    }
+}
---
+++ week2-028.FromHundredToOne/src/FromHundredToOne.java
@@ -0,0 +1,13 @@
+
+public class FromHundredToOne {
+
+    public static void main(String[] args) {
+        // Write your program here
+        int x = 100;
+        while (x >= 1)
+        {
+            System.out.println(x);
+            x--;
+        }
+    }
+}
